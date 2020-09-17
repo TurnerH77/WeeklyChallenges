c572b2ec4017c7baa95a01c8c795577e53e52de9
turner harris <turnerharris7798@yahoo.com>
2020-09-17 10:58:58 -0400

    finished week 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,31 +18,58 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
+            double subNum = (minuend - subtrahend);
+            return subNum;
             throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
+            int addedNum = (number1 + number2);
+            return addedNum;
             throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
+            if(number1 > number2)
+            {
+                return number2;
+            }
+            else if(number2 > number1)
+            {
+                return number1;
+            }
+            else if (number1 == number2)
+            { return number1; }
             throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
+            long multipNum = (factor1 * factor2);
+            return multipNum;
             throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            string greeting = "";
+            if (nameOfPerson == "")
+            {
+                 greeting = "Hello!";
+            }
+            else
+            {
+                greeting = "Hello, " + nameOfPerson + "!";
+            }
+            return greeting;
         }
 
         public string GetHey()
         {
+            string hey = "HEY!";
+            return hey;
             throw new NotImplementedException();
         }
     }
--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,51 +9,186 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
+            if(c.ToString().Length == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
+        }
+
+        public bool CharacterIsNotALetter(char c)
+        {
+            if (c >= 'A' && c <= 'Z') 
+            {
+                return true;
+            }
+            else if(c >= 'a' && c <= 'z')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
         {
+            if(number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public bool IsNumberOdd(int num)
         {
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
             throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int temp1 = 0;
+            int temp = numbers.Count();
+            numbers.DefaultIfEmpty(temp1);
+            if (temp > 0)
+            {
+                double sum = numbers.Max() + numbers.Min();
+                return sum;
+            }
+            else if(temp == 0)
+            {
+                return 0;
+            }
+            
             throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
+            if (str1.Length >= str2.Length)
+            {
+                return str2.Length;
+            }
+            else if(str1.Length <= str2.Length)
+            {
+                return str1.Length;
+            }
             throw new NotImplementedException();
         }
 
         public int Sum(int[] numbers)
         {
+            int num = 0;
+            if(numbers == null)
+            {
+                return 0;
+            }
+            foreach (var item in numbers)
+            {
+                num += item;
+            }
+                return num;
+
             throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
         {
+            int sum = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            foreach (var item in numbers)
+            {
+                if(item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+
             throw new NotImplementedException();
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
+            int sum = 0;
+            if(numbers == null)
+            {
+                return false;
+            }
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            if(sum % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
+            if(number < 0)
+            {
+                return 0;
+            }
+
+            //why this no work?
+            //----------------------------
+            /*long count = 0;
+            for (long i = 1; i < number; i++)
+            {
+                number -= i;
+                if (number >= 0 && number % 2 != 0)
+                {
+                    count += 1;
+                }
+            }
+            return count;*/
+
+            return number / 2;
+
             throw new NotImplementedException();
         }
     }
